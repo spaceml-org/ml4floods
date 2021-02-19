6de6b52af7c0ad5694562df75e3b872c2bd41636
Emmanuel Johnson <emanjohnson91@gmail.com>
2021-02-19 12:06:49 +0000

    Fused example of dataclass.

--- src/data/worldfloods/dataclass_/baseclass.py
+++ src/data/worldfloods/dataclass_/baseclass.py
@@ -4,7 +4,64 @@ from typing import Dict
 
 
 @dataclass
-class WorldFloodsImage:
+class WorldFloodsS2Image:
+    """Simple WFImage dataclass"""
+
+    # ESSENTIAL METADATA
+    filename: str
+    bucket_name: str
+    full_path: str = field(default="Not Specified")
+
+    # PAYLOAD
+    #     source_tiff: bytea = field(default = "Not Specified")
+    meta_data: Dict = field(default="Not Specified")
+
+    # BREADCRUMBS
+    load_date: str = field(default=str(datetime.now()))
+    viewed_by: list = field(default_factory=list, compare=False, repr=False)
+    source_system: str = field(default="Not Specified")
+
+
+@dataclass
+class WorldFloodsL8Image:
+    """Simple WFImage dataclass"""
+
+    # ESSENTIAL METADATA
+    filename: str
+    bucket_name: str
+    full_path: str = field(default="Not Specified")
+
+    # PAYLOAD
+    #     source_tiff: bytea = field(default = "Not Specified")
+    meta_data: Dict = field(default="Not Specified")
+
+    # BREADCRUMBS
+    load_date: str = field(default=datetime.now())
+    viewed_by: list = field(default_factory=list, compare=False, repr=False)
+    source_system: str = field(default="Not Specified")
+
+
+@dataclass
+class WorldFloodsL8Image:
+    """Simple WFImage dataclass"""
+
+    # ESSENTIAL METADATA
+    filename: str
+    bucket_name: str
+    full_path: str = field(default="Not Specified")
+
+    # PAYLOAD
+    #     source_tiff: bytea = field(default = "Not Specified")
+    meta_data: Dict = field(default="Not Specified")
+
+    # BREADCRUMBS
+    load_date: str = field(default=datetime.now())
+    viewed_by: list = field(default_factory=list, compare=False, repr=False)
+    source_system: str = field(default="Not Specified")
+
+
+@dataclass
+class WorldFloodsCloudProb:
     """Simple WFImage dataclass"""
 
     # ESSENTIAL METADATA
