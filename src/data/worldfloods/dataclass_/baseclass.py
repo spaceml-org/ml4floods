854fcc8d172c370e9e0ab78266132ecf1ac0c44f
Emmanuel Johnson <emanjohnson91@gmail.com>
2021-02-22 10:34:34 +0000

    Removed redundant class.

--- src/data/worldfloods/dataclass_/baseclass.py
+++ src/data/worldfloods/dataclass_/baseclass.py
@@ -42,25 +42,6 @@ class WorldFloodsL8Image:
 
 
 @dataclass
-class WorldFloodsL8Image:
-    """Simple WFImage dataclass"""
-
-    # ESSENTIAL METADATA
-    filename: str
-    bucket_name: str
-    full_path: str = field(default="Not Specified")
-
-    # PAYLOAD
-    #     source_tiff: bytea = field(default = "Not Specified")
-    meta_data: Dict = field(default="Not Specified")
-
-    # BREADCRUMBS
-    load_date: str = field(default=datetime.now())
-    viewed_by: list = field(default_factory=list, compare=False, repr=False)
-    source_system: str = field(default="Not Specified")
-
-
-@dataclass
 class WorldFloodsCloudProb:
     """Simple WFImage dataclass"""
 
