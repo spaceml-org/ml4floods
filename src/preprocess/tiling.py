a43cd6383a7992a8f7da79e8c2b2467a6dd18e3b
Emmanuel Johnson <emanjohnson91@gmail.com>
2021-02-22 10:31:32 +0000

    Updated save tiling function. * simple test function for the future.

--- src/preprocess/tiling.py
+++ src/preprocess/tiling.py
@@ -70,9 +70,21 @@ def save_tiles(
     dest_dir: str,
     bands: List[str],
     window_size: WindowSize,
-    transform: Optional[Callable] = None,
     verbose: bool = False,
+    n_samples: Optional[int] = None,
 ) -> None:
+    """does tiling from a predefined window size and saves them to a directory
+
+    Args:
+        file_name (str): the filename to be opened by rasterio
+        dest_dir (str): the directory where the data will be saved
+        bands (List[str]): a list of bands to be accessed and saved
+        window_size (WindowSize): a namedtuple with the height and width
+        verbose (bool): flag to allow one to view the files saved
+
+    Returns:
+        None
+    """
     with rasterio.open(file_name) as dataset:
 
         itile = 0
@@ -88,29 +100,29 @@ def save_tiles(
             window_meta["width"] = window_tile.width
             window_meta["height"] = window_tile.height
             window_meta["channels"] = bands
+            window_meta["transform"] = windows.transform(window_tile, dataset.transform)
 
             # open the dataset with only the window selected
             sub_image = dataset.read(indexes=bands, window=window_tile)
 
-            if sub_image.shape != (len(bands), 128, 128):
-                continue
-            else:
-                # create unique filename for the tile
-                window_file_name = f"{str(Path(file_name).stem)}_tile_{itile}{str(Path(file_name).suffix)}"
+            # create unique filename for the tile
+            window_file_name = (
+                f"{str(Path(file_name).stem)}_tile_{itile}{str(Path(file_name).suffix)}"
+            )
 
-                # filepath for saving
-                output_tile_file_name = str(Path(dest_dir).joinpath(window_file_name))
+            # filepath for saving
+            output_tile_file_name = str(Path(dest_dir).joinpath(window_file_name))
 
-                # open file and also save meta data
-                with rasterio.open(output_tile_file_name, "w", **window_meta) as out_f:
-                    out_f.write(
-                        dataset.read(
-                            window=window_tile,
-                        )
-                    )
+            # open file and also save meta data
+            with rasterio.open(output_tile_file_name, "w", **window_meta) as out_f:
+                out_f.write(
+                    dataset.read(window=window_tile, boundless=True, fill_value=0)
+                )
 
-                itile += 1
-                if verbose:
-                    print(f"Saved: {window_file_name}")
+            itile += 1
+            if verbose:
+                print(f"Saved: {window_file_name}")
 
+            if n_samples is not None and n_samples == itile:
+                break
     return None
