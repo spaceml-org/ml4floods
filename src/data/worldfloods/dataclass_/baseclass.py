256c48d94bb0537e9e519ea15ceb110038b4efb9
Emmanuel Johnson <emanjohnson91@gmail.com>
2021-02-23 11:05:39 +0000

    Demo dataclass * currently converting all tiffs to dataclasses now.

--- src/data/worldfloods/dataclass_/baseclass.py
+++ src/data/worldfloods/dataclass_/baseclass.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Dict
+import numpy as np
 
 
 @dataclass
@@ -14,6 +15,25 @@ class WorldFloodsS2Image:
 
     # PAYLOAD
     #     source_tiff: bytea = field(default = "Not Specified")
+    meta_data: Dict = field(default="Not Specified")
+
+    # BREADCRUMBS
+    load_date: str = field(default=str(datetime.now()))
+    viewed_by: list = field(default_factory=list, compare=False, repr=False)
+    source_system: str = field(default="Not Specified")
+
+
+@dataclass
+class WorldFloodsS2ImageSaved:
+    """Simple WFImage dataclass"""
+
+    # ESSENTIAL METADATA
+    filename: str
+    bucket_name: str
+    full_path: str = field(default="Not Specified")
+
+    # PAYLOAD
+    source_tiff: np.ndarray = field(default="Not Specified")
     meta_data: Dict = field(default="Not Specified")
 
     # BREADCRUMBS
