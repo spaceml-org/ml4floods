c5458786a4347b2cbf04e73575e6a106ba97684c
Gonzalo Mateo <gonzmg88@gmail.com>
2021-10-26 00:47:46 +0200

    Include destination path in main

--- scripts/worldfloods/generate_ground_truth_wf.py
+++ scripts/worldfloods/generate_ground_truth_wf.py
@@ -8,21 +8,25 @@ import os
 import fsspec
 import json
 from typing import Callable, List
-import warnings
 import pkg_resources
 
 
-def main(version="v1_0",overwrite=False, prod_dev="0_DEV", dataset="original", cems_code="", aoi_code=""):
+def main(version="v1_0",overwrite=False, prod_dev="0_DEV", dataset="extra", cems_code="", aoi_code="",
+         destination_parent_path=""):
 
     assert version in ["v1_0", "v2_0"], f"Unexpected ground truth version {version}"
     assert prod_dev in ["0_DEV", "2_PROD"], f"Unexpected environment {prod_dev}"
-    assert dataset in ["original", "extra"], f"Unexpected dataset {dataset}"
+    assert dataset in ["", "original", "extra"], f"Unexpected dataset {dataset}"
 
     destination_bucket_id = "ml4cc_data_lake"
-    if dataset == "original":
-        destination_parent_path = f"{prod_dev}/2_Mart/worldfloods_{version}"
-    else:
-        destination_parent_path = f"{prod_dev}/2_Mart/worldfloods_{dataset}_{version}"
+    if not destination_parent_path:
+        if dataset == "":
+            destination_parent_path = f"{prod_dev}/2_Mart/worldfloods_{version}"
+        elif dataset == "original":
+            destination_parent_path = f"{prod_dev}/2_Mart/worldfloods_{dataset}_{version}"
+        else:
+            destination_parent_path = f"{prod_dev}/2_Mart/worldfloods_{dataset}_{version}"
+
     if version.startswith("v1"):
         gt_fun = generate_land_water_cloud_gt
     elif version.startswith("v2"):
@@ -30,13 +34,12 @@ def main(version="v1_0",overwrite=False, prod_dev="0_DEV", dataset="original", c
     else:
         raise NotImplementedError(f"version {version} not implemented")
 
-    if dataset == "original":
+    if dataset == "":
         main_worldlfoods_original(destination_bucket_id, destination_parent_path, overwrite, gt_fun)
-
-    if dataset == "extra":
+    else:
         staging_path = f"gs://ml4cc_data_lake/{prod_dev}/1_Staging/WorldFloods"
         train_test_split_file = pkg_resources.resource_filename("ml4floods",
-                                                                "data/configuration/train_test_split_extra_dataset.json")
+                                                                f"data/configuration/train_test_split_{dataset}_dataset.json")
         fs_ml4cc = fsspec.filesystem("gs", requester_pays=True)
         files_metadata_pickled = [f"gs://{f}" for f in
                                   fs_ml4cc.glob(f"{staging_path}/*{cems_code}/*{aoi_code}/flood_meta/*.pickle")]
@@ -45,7 +48,6 @@ def main(version="v1_0",overwrite=False, prod_dev="0_DEV", dataset="original", c
                                train_test_split_file=train_test_split_file,
                                overwrite=overwrite, files_metadata_pickled=files_metadata_pickled,
                                gt_fun=gt_fun)
-
 
 
 def main_worldlfoods_extra(destination_path:str,
@@ -150,8 +152,8 @@ def main_worldlfoods_original(destination_bucket_id, destination_parent_path, ov
                     cloudprob_path_dest, floodmap_path_dest, gt_path_dest, \
                     meta_parent_path, permanent_water_image_path_dest, s2_image_path_dest = worldfloods_output_files(
                         output_path=path_write, file_name=s2path.file_name, permanent_water_available=True)
-                    dict_splits[ipath]["S2"].append(s2_image_path_dest.full_path)
-                    dict_splits[ipath]["gt"].append(gt_path_dest.full_path)
+                    dict_splits[ipath]["S2"].append(s2_image_path_dest)
+                    dict_splits[ipath]["gt"].append(gt_path_dest)
                 else:
                     problem_files.append(path_write)
 
@@ -170,8 +172,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser('Generate WorldFloods ML Dataset')
     parser.add_argument('--version', default='v1_0', choices=["v1_0", "v2_0"],
                         help="Which version of the ground truth we want to create (3-class) or multioutput binary")
-    parser.add_argument('--dataset', default='original', choices=["original", "extra"],
-                        help="Use the original data or the newly downloaded data from Copernicus EMS")
+    parser.add_argument('--dataset', default='extra', choices=["", "original", "extra"],
+                        help="Use the old data '', the old data with the new pre-processing 'original' data or "
+                             "the newly downloaded data from Copernicus EMS with new pre-processing 'extra'")
     parser.add_argument('--prod_dev', default='0_DEV', choices=["0_DEV", "2_PROD"],
                         help="environment where the dataset would be created")
     parser.add_argument('--overwrite', default=False, action='store_true',
@@ -182,8 +185,10 @@ if __name__ == "__main__":
     parser.add_argument('--aoi_code', default="",
                         help="CEMS AoI to download images from. If empty string (default) download the images"
                              "from all the AoIs")
+    parser.add_argument('--destination_path', default="",
+                        help="Destination path")
 
     args = parser.parse_args()
 
     main(version=args.version, overwrite=args.overwrite, prod_dev=args.prod_dev, dataset=args.dataset,
-         cems_code=args.cems_code, aoi_code=args.aoi_code)
+         cems_code=args.cems_code, aoi_code=args.aoi_code,destination_parent_path=args.destination_path)
