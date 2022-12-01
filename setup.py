e429f5e0384cd77a3b7e57f32275ab5d501cd6f2
Gonzalo Mateo <gonzmg88@gmail.com>
2022-12-01 11:49:38 +0100

    publish the web page

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+import codecs
+import os.path
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 
 def parse_requirements_file(filename):
@@ -6,6 +11,19 @@ def parse_requirements_file(filename):
         requires = [l.strip() for l in fid.readlines() if l]
     return requires
 
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 # Optional Packages
 # See https://godatadriven.com/blog/a-practical-guide-to-using-setup-py/
@@ -24,8 +42,10 @@ EXTRAS = {
 }
 
 setup(name="ml4floods",
-      version="0.0.1",
+      version=get_version("ml4floods/__init__.py"),
       author="SpaceML-org",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       packages=find_packages(".", exclude=["tests"]),
       package_data={
         "ml4floods" : ["models/configurations/*.json",
